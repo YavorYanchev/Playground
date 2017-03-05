c713c048caec0406fbe93abe40d16a97cd7c1672
YavorYanchev <yavor_yanchev_bs@abv.bg>
2017-03-05 15:13:38 +0200

    Implemented grouping by enrollmentDate in the about page

--- ASP.NET-MVC/ContosoUniversity/ContosoUniversity/ContosoUniversity/ViewModels/EnrollmentDateGroup.cs
+++ ASP.NET-MVC/ContosoUniversity/ContosoUniversity/ContosoUniversity/ViewModels/EnrollmentDateGroup.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,9 +8,9 @@ namespace ContosoUniversity.ViewModels
 {
     public class EnrollmentDateGroup
     {
-        
-        //public DateTime? EnrollmentDate { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime? EnrollmentDate { get; set; }
 
-        //public int StudentCount { get; set; }
+        public int StudentCount { get; set; }
     }
 }
