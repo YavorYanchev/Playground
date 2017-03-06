ea258bc7643715090f20a7573fe81391a5816ceb
YavorYanchev <yavor_yanchev_bs@abv.bg>
2017-03-06 19:44:51 +0200

    Added test to check the status

--- ASP.NET-MVC/ContosoUniversity/ContosoUniversity/ContosoUniversity.Tests/UnitTest1.cs
+++ ASP.NET-MVC/ContosoUniversity/ContosoUniversity/ContosoUniversity.Tests/UnitTest1.cs
@@ -9,6 +9,13 @@ namespace ContosoUniversity.Tests
         [TestMethod]
         public void TestMethod1()
         {
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Assert.IsFalse(false);
         }
     }
 }
