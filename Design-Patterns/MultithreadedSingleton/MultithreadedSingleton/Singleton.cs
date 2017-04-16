9d754f9272e034e0948203a2bfb614e0269a0237
YavorYanchev <yavor_yanchev_bs@abv.bg>
2017-04-16 15:32:25 +0300

    Added singleton

---
+++ Design-Patterns/MultithreadedSingleton/MultithreadedSingleton/Singleton.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultithreadedSingleton
+{
+    public sealed class Singleton
+    {
+        private static volatile Singleton instance;
+        private static object syncRoot = new object();
+
+        private Singleton() { }
+
+        public static Singleton Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    lock (syncRoot)
+                    {
+                        if (instance == null)
+                        {
+                            instance = new Singleton();
+                        }
+                    }
+                }
+                return instance;
+            }
+        }
+
+    }
+}
---
+++ Design-Patterns/MultithreadedSingleton/MultithreadedSingleton/Startup.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultithreadedSingleton
+{
+    public class Startup
+    {
+        public static void Main()
+        {
+            var singleton = Singleton.Instance;
+            //Console.WriteLine(singleton.ToString());
+        }
+    }
+}
