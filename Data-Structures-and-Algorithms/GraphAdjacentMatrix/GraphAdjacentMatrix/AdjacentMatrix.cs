29a1222cc3485f21226a646dcd598fa92b966dfe
YavorYanchev <yavor_yanchev_bs@abv.bg>
2017-04-17 18:46:08 +0300

    Adjacent Matrix

---
+++ Data-Structures-and-Algorithms/GraphAdjacentMatrix/GraphAdjacentMatrix/AdjacentMatrix.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraphAdjacentMatrix
+{
+    public class AdjacentMatrix
+    {
+        public static void Main(string[] args)
+        {
+            int nodes = int.Parse(Console.ReadLine());
+
+            var graph = new int[nodes, nodes];
+
+            for (int i = 0; i < nodes; i++)
+            {
+                string[] connections = Console.ReadLine().Split(' ');
+                foreach (var connection in connections)
+                {
+                    graph[i, int.Parse(connection)] = 1;
+                }
+            }
+
+            for (int i = 0; i < nodes; i++)
+            {
+                for (int j = 0; j < nodes; j++)
+                {
+                    Console.Write(graph[i, j] + " ");
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
