060b399e25c09543b03d4d1f0b7d6d97819c55de
YavorYanchev <yavor_yanchev_bs@abv.bg>
2017-04-17 18:23:14 +0300

    Stack implementation

---
+++ Data-Structures-and-Algorithms/StackImplementation/StackImplementation/Stack.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace StackImplementation
+{
+    public class Stack<T>
+    {
+        private T[] stack;
+        private int nextFreeStackIndex;
+
+        public Stack(int stackSize)
+        {
+            if (stackSize < 0)
+            {
+                throw new ArgumentOutOfRangeException("Size of the stack must be greater than 0");
+            }
+            stack = new T[stackSize];
+            nextFreeStackIndex = 0;
+        }
+
+        public void Push(T value)
+        {
+            if (nextFreeStackIndex == stack.Length)
+            {
+                Array.Resize(ref stack, stack.Length < 100 ? 2 * stack.Length : (int)(stack.Length * 1.2));
+            }
+
+            stack[nextFreeStackIndex++] = value;
+        }
+
+        public T Pop()
+        {
+            if (nextFreeStackIndex == 0)
+            {
+                throw new InvalidOperationException("The stack is empty");
+            }
+
+            T elementToPop = stack[--nextFreeStackIndex];
+
+            return elementToPop;
+        }
+    }
+}
---
+++ Data-Structures-and-Algorithms/StackImplementation/StackImplementation/Startup.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace StackImplementation
+{
+    public class Startup
+    {
+        public static void Main()
+        {
+            var myStack = new Stack<int>(5);
+            myStack.Push(1);
+            myStack.Push(2);
+            Console.WriteLine(myStack.Pop());
+            Console.WriteLine(myStack.Pop());
+        }
+    }
+}
