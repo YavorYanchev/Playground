1c6be6eb7b729a4ff2d8134a48da4ed01c540985
YavorYanchev <yavor_yanchev_bs@abv.bg>
2017-04-18 20:19:10 +0300

    Added bubble sort

---
+++ Data-Structures-and-Algorithms/BubbleSort/BubbleSort/Startup.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace BubbleSort
+{
+    public class Startup
+    {
+        public static void Main(string[] args)
+        {
+            int[] arr = { 89, 23, 65, 12, 543, 62, 3, 90 };
+            int elementsCount = arr.Length;
+            int temp;
+            for (int i = 0; i < elementsCount; i++)
+            {
+                for (int j = 0; j < elementsCount-1; j++)
+                {
+                    if (arr[j] > arr[j+1])
+                    {
+                        temp = arr[j + 1];
+                        arr[j + 1] = arr[j];
+                        arr[j] = temp;
+                    }
+                }
+            }
+            foreach (var item in arr)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
