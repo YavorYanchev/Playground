1c90b26e09c9d510e1f82822193dc3effc7e3f9c
YavorYanchev <yavor_yanchev_bs@abv.bg>
2017-04-24 22:51:39 +0300

    binary search added

---
+++ Data-Structures-and-Algorithms/BinarySearchImplementation/BinarySearchImplementation/SortableCollection.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchImplementation
+{
+    public class SortableCollection<T> where T : IComparable<T>
+    {
+        private readonly IList<T> items;
+
+        public SortableCollection()
+        {
+            this.items = new List<T>();
+        }
+
+        public SortableCollection(IEnumerable<T> items)
+        {
+            this.items = new List<T>(items);
+        }
+
+
+        public bool BinarySearch(T item)
+        {
+            var left = 0;
+            var right = this.items.Count - 1;
+            while (left <= right)
+            {
+                var mid = (left + right) / 2;
+                if (this.items[mid].CompareTo(item) < 0)
+                {
+                    left = mid + 1;
+                }
+                else if (this.items[mid].CompareTo(item) > 0)
+                {
+                    right = mid - 1;
+                }
+                else if (this.items[mid].CompareTo(item) == 0)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ Data-Structures-and-Algorithms/BinarySearchImplementation/BinarySearchImplementation/Startup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchImplementation
+{
+    public class Startup
+    {
+        public static void Main(string[] args)
+        {
+            //sorted collection
+            var collection = new SortableCollection<int>(new[] { 1, 5, 12, 45, 376, 456, 567, 678, 789, 1634, 3647 });
+            Console.WriteLine(collection.BinarySearch(376));
+        }
+    }
+}
